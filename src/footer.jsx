234730712c9657d81bc51566d5271a32f66ca441
Bada tomiwa <tomiwabd@gmail.com>
2022-10-29 17:11:25 -0400

    Add footer component

---
+++ src/footer.jsx
@@ -0,0 +1,14 @@
+import zuri from './assets/zuri.svg'
+import i4g from './assets/I4G.svg'
+
+function Footer(){
+    return(
+        <footer>
+        <div><img src={zuri} /></div>
+        <p>HNG Internship 9 Frontend Task</p>
+        <div><img src={i4g} /></div>
+        </footer>
+    )
+}
+
+export default Footer
