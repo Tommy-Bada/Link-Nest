fe58f14ccd18f3de17f96e6eec0f51bb1e0525ee
Bada tomiwa <tomiwabd@gmail.com>
2022-10-30 16:11:41 -0400

    Fix link componet embeded bug

--- src/links.jsx
+++ src/links.jsx
@@ -4,12 +4,12 @@ import github from './assets/github.svg'
 function Links(){
     return(
         <main>
-        <div><a href="https://twitter.com/Badman_Bada"  id="twitter__link"><button>Twitter</button></a></div>
-        <div><a href="https://training.zuri.team/" id="btn__zuri" ><button>Zuri Team</button></a></div>
-        <div><a href="http://books.zuri.team/" id="books"><button>Zuri Books</button></a></div>
-        <div><a href="https://books.zuri.team/python-for-beginners?ref_id=BadaScript" id="book__python" ><button>Python Books</button></a></div>
-        <div><a href="https://background.zuri.team/" id="pitch"><button>Background Check for Coders</button></a></div>
-        <div><a href="https://books.zuri.team/design-rules" id="book__design" ><button>Design Books</button></a></div>
+        <a href="https://twitter.com/Badman_Bada"  id="twitter__link">Twitter</a>
+        <a href="https://training.zuri.team/" id="btn__zuri" >Zuri Team</a>
+        <a href="http://books.zuri.team/" id="books">Zuri Books</a>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=BadaScript" id="book__python" >Python Books</a>
+        <a href="https://background.zuri.team/" id="pitch">Background Check for Coders</a>
+        <a href="https://books.zuri.team/design-rules" id="book__design" >Design Books</a>
         <div className='link-logos'>
             <div><img src={slack} /></div>
             <div><img src={github} /></div>
