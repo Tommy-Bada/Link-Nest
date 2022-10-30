ea1873144c9be1154d5134e002100ac3dc25ccc2
Bada tomiwa <tomiwabd@gmail.com>
2022-10-30 13:41:32 -0400

    Fix Header image ID

--- src/header.jsx
+++ src/header.jsx
@@ -3,7 +3,7 @@ import profilePicture from './assets/profile-picture.png'
 function Header(){
     return(
         <header>
-        <div><img src={profilePicture} id="profile_img" /></div>
+        <div><img src={profilePicture} id="profile__img" /></div>
         <h2 id="twitter">Badman_Bada</h2>
         <h3 id="slack">BadaScript</h3>
         </header>
