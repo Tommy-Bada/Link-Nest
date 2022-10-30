9e10f9257fee1edcdc0bbb289ae9cae54e82869f
Bada tomiwa <tomiwabd@gmail.com>
2022-10-30 16:40:28 -0400

    Add title and fix subtext of anchors

--- src/links.jsx
+++ src/links.jsx
@@ -4,12 +4,12 @@ import github from './assets/github.svg'
 function Links(){
     return(
         <main>
-        <a href="https://twitter.com/Badman_Bada"  id="twitter__link">Twitter</a>
+        <a href="https://twitter.com/Badman_Bada" id="twitter__link">Twitter</a>
         <a href="https://training.zuri.team/" id="btn__zuri" >Zuri Team</a>
-        <a href="http://books.zuri.team/" id="books">Zuri Books</a>
-        <a href="https://books.zuri.team/python-for-beginners?ref_id=BadaScript" id="book__python" >Python Books</a>
-        <a href="https://background.zuri.team/" id="pitch">Background Check for Coders</a>
-        <a href="https://books.zuri.team/design-rules" id="book__design" >Design Books</a>
+        <a href="http://books.zuri.team/" title="This is where I get books relating to design and coding" id="books">Get Amazing Zuri Books</a>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=BadaScript" id="book__python" >Get Amazing Books for Python</a>
+        <a href="https://background.zuri.team/" id="pitch">Check Coders Background</a>
+        <a href="https://books.zuri.team/design-rules" id="book__design" >Get Zuri Design Books</a>
         <div className='link-logos'>
             <div><img src={slack} /></div>
             <div><img src={github} /></div>
