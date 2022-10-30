f584f4438e1066264a46adfd4055137cdba0f652
Bada tomiwa <tomiwabd@gmail.com>
2022-10-30 13:42:27 -0400

    Fix link component anchor IDs

--- src/links.jsx
+++ src/links.jsx
@@ -4,12 +4,12 @@ import github from './assets/github.svg'
 function Links(){
     return(
         <main>
-        <div><a href="https://twitter.com/Badman_Bada" ><button id="twitter-link" >Twitter</button></a></div>
-        <div><a href="https://training.zuri.team/" ><button id="btn_zuri" >Zuri Team</button></a></div>
-        <div><a href="http://books.zuri.team/" ><button id="books" >Zuri Books</button></a></div>
-        <div><a href="https://books.zuri.team/python-for-beginners?ref_id=BadaScript" ><button id="book_python" >Python Books</button></a></div>
-        <div><a href="https://background.zuri.team/" ><button id="pitch" >Background Check for Coders</button></a></div>
-        <div><a href="https://books.zuri.team/design-rules" ><button id="book_design">Design Books</button></a></div>
+        <div><a href="https://twitter.com/Badman_Bada"  id="twitter__link"><button>Twitter</button></a></div>
+        <div><a href="https://training.zuri.team/" id="btn__zuri" ><button>Zuri Team</button></a></div>
+        <div><a href="http://books.zuri.team/" id="books"><button>Zuri Books</button></a></div>
+        <div><a href="https://books.zuri.team/python-for-beginners?ref_id=BadaScript" id="book__python" ><button>Python Books</button></a></div>
+        <div><a href="https://background.zuri.team/" id="pitch"><button>Background Check for Coders</button></a></div>
+        <div><a href="https://books.zuri.team/design-rules" id="book__design" ><button>Design Books</button></a></div>
         <div className='link-logos'>
             <div><img src={slack} /></div>
             <div><img src={github} /></div>
