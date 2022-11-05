5555d48119d6ff7f46eb66e99719dee5fda88aaf
Bada tomiwa <tomiwabd@gmail.com>
2022-11-05 08:26:20 -0400

    Add a contact component

---
+++ src/contact.jsx
@@ -0,0 +1,12 @@
+import Form from './form'
+import Footer from './footer'
+
+
+export default function Contact() {
+    return (
+        <>
+        <Form />
+        <Footer />
+        </>
+    );
+  }
